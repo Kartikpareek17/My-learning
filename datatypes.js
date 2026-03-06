29505d2bdde2686b2bb87c44ba849cd50a312dbc
Kartik <kartikpareek207@gmail.com>
2026-03-06 21:50:19 +0530

    Check type of differnt datatypes

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,16 @@
-console.log("hi")
-console.log("hello")
-console.log("hii")
+const scoreValue = 100
+const A = null
+const B = undefined
+const Movies = ["Raone", "kgf"]
+const MyIntro = {
+    Name: "Kartik",
+    Age: 22
+
+
+}
+
+console.log(typeof MyIntro)
+console.log(typeof Movies)
+console.log(typeof A)
+console.log(typeof B)
+console.log(typeof scoreValue)
